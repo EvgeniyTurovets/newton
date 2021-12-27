02db7542e14c12ea34e7cc671cc0351db822fb59
sanyokstd <sanyokstd@gmail.com>
2021-12-27 16:02:41 +0200

    1
    1

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log('global');
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {});
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {});
+"use strict";
+//# sourceMappingURL=main.js.map
